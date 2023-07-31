5080abda6f6c723e8af757411aff77861bfc8d82
mazr26 <95585964+mazr26@users.noreply.github.com>
2023-07-31 06:18:28 -0700

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x879eabb17f94a4b7ca5f5e87f6e3eb73d59d18c0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
